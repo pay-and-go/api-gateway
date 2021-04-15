d0e3d78fd2509a0315c0ce82301244ef6b99c69a
José Organista <jodorganistaca@unal.edu.co>
2021-04-14 20:09:06 -0500

    :wrench: update files for prepare for merge

--- src/gateway/navigation/resolverNavigation.js
+++ src/gateway/navigation/resolverNavigation.js
@@ -7,7 +7,7 @@ const URL = `http://${url}:${port}/`;
 const resolvers = {
     Mutation: {
         findToll: async (_, { coor_lat, coor_lng }) => {
-            console.log("llego hasta aqui 1");
+            // console.log("llego hasta aqui 1");
             const tolls = await tollResolver.Query.allTolls();
             return await generalRequest(`${URL}findToll`, 'POST', {"coor_lat": coor_lat, "coor_lng": coor_lng, "tolls": tolls});
         }
--- src/gateway/navigation/serverNavigation.js
+++ src/gateway/navigation/serverNavigation.js
@@ -1,5 +1,3 @@
-// export const url = '18.209.131.13'
-// export const port = '80'
-export const url = '127.0.0.1'
-export const port = '3001'
+export const url = '18.209.131.13'
+export const port = '80'
 
